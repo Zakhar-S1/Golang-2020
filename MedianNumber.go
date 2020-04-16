a7a7116e9f3483b1ca9df573bfab944017f9e321
Zakhar-S1 <43477527+Zakhar-S1@users.noreply.github.com>
2020-04-16 18:39:30 +0300

    Create MedianNumber.go

---
+++ MedianNumber.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func medianNumber(i []int) float64 {
+
+	if len(i)%2 == 0 {
+		return float64(i[len(i)/2] + i[len(i)/2-1]) / 2
+	}
+
+	return float64(i[len(i)/2])
+}
+
+func main() {
+
+	fmt.Println(medianNumber([]int{1, 2, 3, 4, 5}))
+	fmt.Println(medianNumber([]int{3, 3, 5, 6, 9, 11}))
+	fmt.Println(medianNumber([]int{1, 2, 2, 6}))
+
+}

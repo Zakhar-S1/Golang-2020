41fba4eaa249c6f113fb14b2e8e43e7707f7f2de
Zakhar-S1 <43477527+Zakhar-S1@users.noreply.github.com>
2020-04-18 15:04:13 +0300

    Update MedianNumber.go

--- MedianNumber.go
+++ MedianNumber.go
@@ -1,8 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func medianNumber(i []int) float64 {
+	
+	sort.Ints(i)
 
 	if len(i)%2 == 0 {
 		return float64(i[len(i)/2] + i[len(i)/2-1]) / 2
@@ -16,5 +21,7 @@ func main() {
 	fmt.Println(medianNumber([]int{1, 2, 3, 4, 5}))
 	fmt.Println(medianNumber([]int{3, 3, 5, 6, 9, 11}))
 	fmt.Println(medianNumber([]int{1, 2, 2, 6}))
+	fmt.Println(medianNumber([]int{4, 3, 8, 5}))
+	fmt.Println(medianNumber([]int{9, 5, 1}))
 
 }

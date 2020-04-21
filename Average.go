adc4487cd1dd62f96d69b2bc6edf77a807e242a2
Zakhar-S1 <43477527+Zakhar-S1@users.noreply.github.com>
2020-04-21 14:58:16 +0300

    Create Average.go

---
+++ Average.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func average(a []int) float64{
+	if len(a) == 0 {
+		return 0
+	}
+
+	var sum int = 0 
+
+	for i := 0; i < len(a); i++ {
+		sum += a[i] 
+	}
+
+	return float64(sum) / float64(len(a))
+}
+
+func main(){
+	fmt.Print("Average value of array is ")
+	fmt.Println(average([]int{1, 2, 3, 4, 5, 6}))
+}
